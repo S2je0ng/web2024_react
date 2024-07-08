bf3f45a62a5623dfa5f9cfa75da2dca08fed31bd
Sejeong <3255sera@pusan.ac.kr>
2024-07-08 19:13:25 +0900

    리액트기초

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './01/Hello';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //     <a
+    //       className="App-link"
+    //       href="https://github.com/S2je0ng/web2024_react"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       박세정
+    //     </a>
+    //     </p>
+    //     <Hello />
+    //   </header>
+    // </div>
+
+    <div className='App-header'>
+      <Hello />
     </div>
   );
 }
